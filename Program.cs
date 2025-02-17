a1e3ac09026687554bdabd4be785e75f07018797
Ariel Spencer <arielspencer.tech@gmail.com>
2025-02-16 23:34:06 -0300

    feat: add RegisterBand method to register new bands

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿void PrintLogo()
+﻿List<string> bandList = new List<string>();
+
+void PrintLogo()
 {
   Console.WriteLine( @"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -18,6 +20,66 @@ void PrintMenuOptions()
   Console.WriteLine("3. Rate a band");
   Console.WriteLine("4. Show the average of a band");
   Console.WriteLine("5. Exit");
+
+  Console.Write("\nType the number of the option you want to select: ");
+  string choiceOption = Console.ReadLine()!;
+  int choiceOptionInt = int.Parse(choiceOption);
+
+  switch (choiceOptionInt)
+  {
+    case 1: RegisterBand();
+      break;
+    case 2: ViewAllBands();
+      break;
+    case 3: RateBand();
+      break;
+    case 4: ShowAverage();
+      break;
+    case 5:
+      Console.WriteLine("Bye!");
+      break;
+    default:
+      Console.WriteLine("Invalid option. Please select a valid option.");
+      PrintMenuOptions();
+      break;
+  }
+}
+
+void RegisterBand()
+{
+  Console.Clear();
+  Console.WriteLine("Add a new band!");
+  Console.WriteLine("Please enter the name of the band: ");
+  string bandName = Console.ReadLine()!;
+  bandList.Add(bandName);
+  Console.WriteLine($"You registered the band {bandName}!");
+  Thread.Sleep(2000);
+  Console.Clear();
+  PrintMenuOptions();
+}
+
+void ViewAllBands()
+{
+  Console.WriteLine("In this version, there are no bands registered yet.");
+  Thread.Sleep(2000);
+  Console.Clear();
+  PrintMenuOptions();
+}
+
+void RateBand()
+{
+  Console.WriteLine("In this version, you can't rate a band yet.");
+  Thread.Sleep(2000);
+  Console.Clear();
+  PrintMenuOptions();
+}
+
+void ShowAverage()
+{
+  Console.WriteLine("In this version, you can't see the average of a band yet.");
+  Thread.Sleep(2000);
+  Console.Clear();
+  PrintMenuOptions();
 }
 
 PrintMenuOptions();
